600f772ba5e25fa131f8e014eb49010aaf0ac788
jaber <jaberheidary163@gmail.com>
2024-12-25 10:59:03 +0330

    Remove cloudinary

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,41 +1,41 @@
-"use client";
-import React, { useState } from "react";
-import { CldUploadWidget, CldImage } from "next-cloudinary";
+// "use client";
+// import React, { useState } from "react";
+// import { CldUploadWidget, CldImage } from "next-cloudinary";
 
-interface CloudinaryResult {
-  public_id: string;
-}
+// interface CloudinaryResult {
+//   public_id: string;
+// }
 
-const UploadPage = () => {
-  const [publicId, setPublicId] = useState("");
+// const UploadPage = () => {
+//   const [publicId, setPublicId] = useState("");
 
-  return (
-    <>
-      {publicId && (
-        <CldImage src={publicId} width={270} height={180} alt="A fighter" />
-      )}
-      <CldUploadWidget
-        uploadPreset="zudsjile"
-        options={{
-          sources: ["local"],
-          multiple: false,
-          maxFiles: 5,
-        }}
-        onSuccess={(result, widget) => {
-          console.log(result);
-          // if (result.event !== "success") return;
-          const info = result.info as CloudinaryResult;
-          setPublicId(info.public_id);
-        }}
-      >
-        {({ open }) => (
-          <button onClick={() => open()} className="btn btn-primary">
-            Upload
-          </button>
-        )}
-      </CldUploadWidget>
-    </>
-  );
-};
+//   return (
+//     <>
+//       {publicId && (
+//         <CldImage src={publicId} width={270} height={180} alt="A fighter" />
+//       )}
+//       <CldUploadWidget
+//         uploadPreset="zudsjile"
+//         options={{
+//           sources: ["local"],
+//           multiple: false,
+//           maxFiles: 5,
+//         }}
+//         onSuccess={(result, widget) => {
+//           console.log(result);
+//           // if (result.event !== "success") return;
+//           const info = result.info as CloudinaryResult;
+//           setPublicId(info.public_id);
+//         }}
+//       >
+//         {({ open }) => (
+//           <button onClick={() => open()} className="btn btn-primary">
+//             Upload
+//           </button>
+//         )}
+//       </CldUploadWidget>
+//     </>
+//   );
+// };
 
-export default UploadPage;
+// export default UploadPage;
